a42ea39cfd282336fa97dcddfe68aa700a77a674
David Keller <mrcodeswildride@gmail.com>
2020-06-16 14:46:00 +0000

    es6 update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # calculator
 
-This is a calculator web app written in HTML, CSS, and JavaScript.
+Calculator web app written in HTML, CSS, and JavaScript.
 
 Use this app at [calculator.mrcodeswildride.com](https://calculator.mrcodeswildride.com/).
 Learn how to make this app at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
--- script.js
+++ script.js
@@ -1,61 +1,61 @@
-var display = document.getElementById("display");
-var digits = document.querySelectorAll(".digit");
-var operators = document.querySelectorAll(".operator");
-var equal = document.getElementById("equal");
+let display = document.getElementById(`display`)
+let digits = document.getElementsByClassName(`digit`)
+let operators = document.getElementsByClassName(`operator`)
+let equal = document.getElementById(`equal`)
 
-var firstDigits = "";
-var operator = "";
-var secondDigits = "";
+let firstDigits = ``
+let operator = ``
+let secondDigits = ``
 
-for (var i = 0; i < digits.length; i++) {
-    digits[i].addEventListener("click", selectDigit);
+for (let digit of digits) {
+  digit.addEventListener(`click`, selectDigit)
 }
 
-for (var i = 0; i < operators.length; i++) {
-    operators[i].addEventListener("click", selectOperator);
+for (let op of operators) {
+  op.addEventListener(`click`, selectOperator)
 }
 
-equal.addEventListener("click", selectEqual);
+equal.addEventListener(`click`, selectEqual)
 
 function selectDigit() {
-    display.innerHTML = display.innerHTML + this.innerHTML;
+  display.innerHTML = `${display.innerHTML}${this.innerHTML}`
 
-    if (operator == "") {
-        firstDigits = firstDigits + this.innerHTML;
-    }
-    else {
-        secondDigits = secondDigits + this.innerHTML;
-    }
+  if (operator == ``) {
+    firstDigits = `${firstDigits}${this.innerHTML}`
+  }
+  else {
+    secondDigits = `${secondDigits}${this.innerHTML}`
+  }
 }
 
 function selectOperator() {
-    if (firstDigits != "" && operator == "") {
-        display.innerHTML = display.innerHTML + " " + this.innerHTML + " ";
-        operator = this.getAttribute("id");
-    }
+  if (firstDigits != `` && operator == ``) {
+    display.innerHTML = `${display.innerHTML} ${this.innerHTML} `
+    operator = this.innerHTML
+  }
 }
 
 function selectEqual() {
-    if (secondDigits != "") {
-        var answer;
+  if (secondDigits != ``) {
+    let answer
 
-        if (operator == "plus") {
-            answer = parseFloat(firstDigits) + parseFloat(secondDigits);
-        }
-        else if (operator == "minus") {
-            answer = parseFloat(firstDigits) - parseFloat(secondDigits);
-        }
-        else if (operator == "times") {
-            answer = parseFloat(firstDigits) * parseFloat(secondDigits);
-        }
-        else if (operator == "divide") {
-            answer = parseFloat(firstDigits) / parseFloat(secondDigits);
-        }
-
-        display.innerHTML = isNaN(answer) ? "Error" : answer;
-
-        firstDigits = display.innerHTML;
-        operator = "";
-        secondDigits = "";
+    if (operator == `+`) {
+      answer = Number(firstDigits) + Number(secondDigits)
     }
+    else if (operator == `-`) {
+      answer = Number(firstDigits) - Number(secondDigits)
+    }
+    else if (operator == `×`) {
+      answer = Number(firstDigits) * Number(secondDigits)
+    }
+    else if (operator == `÷`) {
+      answer = Number(firstDigits) / Number(secondDigits)
+    }
+
+    display.innerHTML = answer
+
+    firstDigits = answer
+    operator = ``
+    secondDigits = ``
+  }
 }
